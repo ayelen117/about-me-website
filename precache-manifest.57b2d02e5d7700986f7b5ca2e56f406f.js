2c3f4f39b7699006d4d5b9032c04ad24463bb47a
ayelen.guerra117@gmail.com <ayelen.guerra117@gmail.com>
2020-03-31 12:25:33 -0300

    first commit

---
+++ precache-manifest.57b2d02e5d7700986f7b5ca2e56f406f.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e51667c3ebfdf076dd92",
+    "url": "/about-me-website/css/app.0b095fde.css"
+  },
+  {
+    "revision": "8c4f1751a2474c5c593f",
+    "url": "/about-me-website/css/chunk-vendors.3b3b89f8.css"
+  },
+  {
+    "revision": "508776ad40e95a7803caca45b2332de2",
+    "url": "/about-me-website/img/portfolio/HealthCareForYourCar.png"
+  },
+  {
+    "revision": "c0557a8ed0fc6da7d2291e7f696b7e77",
+    "url": "/about-me-website/img/portfolio/bitmovil.png"
+  },
+  {
+    "revision": "841ed2711b8332e4816415a2ab2d94d9",
+    "url": "/about-me-website/img/portfolio/control-documentario.png"
+  },
+  {
+    "revision": "e1f309386b5c8476783b7f8570bd0fa7",
+    "url": "/about-me-website/img/portfolio/hc4yc.png"
+  },
+  {
+    "revision": "9d4a90f9af7157b3f80c3f321f2c7ea3",
+    "url": "/about-me-website/img/portfolio/multinexo.png"
+  },
+  {
+    "revision": "bab30611238cac118ce5ac8d35db6278",
+    "url": "/about-me-website/img/portfolio/php-afip-ws.jpg"
+  },
+  {
+    "revision": "c4cfae72e46e0557c1fc4ddc331cfaed",
+    "url": "/about-me-website/img/portfolio/prestonwood.png"
+  },
+  {
+    "revision": "07ae1b11dcdc8fd5a575dc0b60d74225",
+    "url": "/about-me-website/img/portfolio/pyfiscalprinter.jpg"
+  },
+  {
+    "revision": "613500ecd1226aa4d2149cd4dca97c97",
+    "url": "/about-me-website/img/portfolio/saldo.png"
+  },
+  {
+    "revision": "6f52caafb1c007b3eba27c2c20cb20fd",
+    "url": "/about-me-website/img/portfolio/smart-drivertek.png"
+  },
+  {
+    "revision": "ade7b7d29e9a474233b3882585f6b880",
+    "url": "/about-me-website/img/portfolio/tesis.png"
+  },
+  {
+    "revision": "a4543d842a4e2ed596979c2ab909b663",
+    "url": "/about-me-website/img/profile.png"
+  },
+  {
+    "revision": "0543f1fb249da4275eeb3e010aa79016",
+    "url": "/about-me-website/index.html"
+  },
+  {
+    "revision": "9fb04cca40c33dfde8fe",
+    "url": "/about-me-website/js/about.3b17fd52.js"
+  },
+  {
+    "revision": "e51667c3ebfdf076dd92",
+    "url": "/about-me-website/js/app.efa970bc.js"
+  },
+  {
+    "revision": "8c4f1751a2474c5c593f",
+    "url": "/about-me-website/js/chunk-vendors.0fe2f588.js"
+  },
+  {
+    "revision": "28e13fa8be9f366ab9eb85b70bea7239",
+    "url": "/about-me-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/about-me-website/robots.txt"
+  },
+  {
+    "revision": "053100cb84a50d2ae7f5492f7dd7f25e",
+    "url": "/about-me-website/site.webmanifest"
+  }
+]);
