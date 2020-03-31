369cad7249b6f57ef06cc90024245768ddf51d11
ayelen.guerra117@gmail.com <ayelen.guerra117@gmail.com>
2020-03-31 11:47:41 -0300

    Travis CI setup

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  lintOnSave: process.env.NODE_ENV !== 'production',
+  lintOnSave: process.env.NODE_ENV !== "production",
   pwa: {
-    name: 'Ayelen Guerra'
-  }
-}
+    name: "Ayelen Guerra"
+  },
+  publicPath: process.env.NODE_ENV === "production" ? "/about-me-website/" : "/"
+};
